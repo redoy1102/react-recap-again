c7f52aefcf5ba3b13b7dc918fee3c75331b45b0f
unknown <mdfuadulislamhridoy52@gmail.com>
2020-12-16 18:10:48 +0600

    33-8 video complete

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Count></Count>
         <Nayok name={details[0]}></Nayok>
         <Nayok name={details[3]} ></Nayok>
         <Nayok name={details[1]}></Nayok>
@@ -14,6 +16,24 @@ function App() {
   );
 }
 
+function Count() {
+  const [count, setCount] = useState(0);
+  return(
+    <div>
+      <h2>Movie Counter: {count}</h2>
+      <button onClick={() => setCount(count+1)} >Increase</button>
+      <MovieCount MovieCount={count + 10} ></MovieCount>
+    </div>
+  );
+}
+
+function MovieCount(props) {
+  return(
+    <div>
+      <h2>I have acted: {props.MovieCount}</h2>
+    </div>
+  );
+}
 function Nayok(props) {
   console.log(props);
   var NayokStyle = {
