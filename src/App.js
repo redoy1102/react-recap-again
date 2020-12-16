9b500500c3830f68398487fc5fa4feb4dbe43955
unknown <mdfuadulislamhridoy52@gmail.com>
2020-12-16 16:30:37 +0600

    33-6 number video done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const details = ['Sakib Khan', 'Manna', 'Dipjol', 'Bapparaz']
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Nayok name={details[0]}></Nayok>
+        <Nayok name={details[3]} ></Nayok>
+        <Nayok name={details[1]}></Nayok>
+        <Nayok name={details[2]}></Nayok>
       </header>
+    </div>
+  );
+}
+
+function Nayok(props) {
+  console.log(props);
+  var NayokStyle = {
+    border: '1px solid red',
+    margin: '5px',
+    width: '400px'
+  }
+  var name = props.name;
+  return(
+    <div style={NayokStyle} >
+      <h1>I am {name}</h1>
+      <p>Tui amare cinos ami {name}</p>
     </div>
   );
 }
