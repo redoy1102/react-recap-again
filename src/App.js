61729e1637c46773efa6120bdcd7b8bdb08fa917
unknown <mdfuadulislamhridoy52@gmail.com>
2020-12-16 18:47:12 +0600

    33-10 number video done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const products = [
+    {name: 'HSC Higher Math CHapter-1 SHIKKHANGON BD Smart PDF', price: '$30', description: 'For HSC Student'},
+    {name: 'HSC Higher Math CHapter-7 SHIKKHANGON BD Smart PDF', price: '$30', description: 'For HSC Student'},
+    {name: 'SSC Higher Math CHapter-9 SHIKKHANGON BD Smart PDF', price: '$30', description: 'For SSC Student'},
+    {name: 'SSC Higher Math CHapter-10 SHIKKHANGON BD Smart PDF', price: '$30', description: 'For SSC Student'},
+    {name: 'SSC Higher Math CHapter-4 SHIKKHANGON BD Smart PDF', price: '$30', description: 'For SSC Student'},
+  ]
   const details = ['Sakib Khan', 'Manna', 'Dipjol', 'Bapparaz']
   return (
     <div className="App">
       <header className="App-header">
         <Count></Count>
+        {
+          products.map(pd => <Product name = {pd.name} price={pd.price} description={pd.description}></Product>)
+        }
+        
         <Nayok name={details[0]}></Nayok>
         <Nayok name={details[3]} ></Nayok>
         <Nayok name={details[1]}></Nayok>
@@ -16,6 +27,21 @@ function App() {
   );
 }
 
+function Product(props){
+  const productStyle = {
+    border: '2px solid red',
+    margin: '5px',
+    padding: '5px',
+    width: '500px'
+  }
+  return(
+    <div style={productStyle}>
+      <h3>{props.name}</h3>
+      <p>{props.price}</p>
+      <p>{props.description}</p>
+    </div>
+  );
+}
 function Count() {
   const [count, setCount] = useState(0);
   return(
